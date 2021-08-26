166d21bc5238374e54d1ac9e0d3901b124106bcf
Franco <franconoronha11@gmail.com>
2021-08-25 22:01:03 -0300

    teste-ex3

--- exercicio1/alomundo.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Alo Mundo!");
-    return 0;
-}
--- testes/teste4.c
+++ testes/teste4.c
@@ -2,24 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-void adiciona_dois(void* pBuffer);
+void adiciona(void* pBuffer);
 
 int main() {
     int a, b, c;
-    void *pBuffer = malloc(sizeof(int) * 3);
-    adiciona_dois(pBuffer);
-    memcpy(&a, pBuffer, sizeof(int));
-    memcpy(&b, pBuffer+sizeof(int), sizeof(int));
-    memcpy(&c, pBuffer+(2*sizeof(int)), sizeof(int));
-    printf("%d %d %d", a, b, c);
+    char d;
+    char nome[10];
+    void *pBuffer = malloc(sizeof(int) * 3 + sizeof(char));
+    adiciona(pBuffer);
+    a = *(int *)pBuffer;
+    b = *(int *)(pBuffer + sizeof(int));
+    c = *(int *)(pBuffer + (2 * sizeof(int)));
+    d = *(char *)(pBuffer + (3 * sizeof(int)));
+    //memcpy(&a, pBuffer, sizeof(int));
+    //memcpy(&b, pBuffer+sizeof(int), sizeof(int));
+    //memcpy(&c, pBuffer+(2*sizeof(int)), sizeof(int));
+    printf("%d %d %d %c", a, b, c, d);
+    pBuffer = realloc(pBuffer, (sizeof(int) + (a * (sizeof(nome) + sizeof(int) * 2))));
+    if(!pBuffer) printf("erro\n");
+    pBuffer = realloc(pBuffer, (sizeof(int) + (b * (sizeof(nome) + sizeof(int) * 2))));
+    if(!pBuffer) printf("erro\n");
+    pBuffer = realloc(pBuffer, (sizeof(int) + (c * (sizeof(nome) + sizeof(int) * 2))));
+    if(!pBuffer) printf("erro\n");
+    free(pBuffer);
     return 0;
 }
 
-void adiciona_dois(void *pBuffer){
+void adiciona(void *pBuffer){
     int a = 10;
     int b = 20;
     int c = 30;
-    memcpy(pBuffer, &a, sizeof(int));
-    memcpy(pBuffer+sizeof(int), &b, sizeof(int));
-    memcpy(pBuffer+(2*sizeof(int)), &c, sizeof(int));
+    char d = 'c';
+    *(int *)pBuffer = a;
+    *(int *)(pBuffer + sizeof(int)) = b;
+    *(int *)(pBuffer + (2 * sizeof(int))) = c;
+    *(char *)(pBuffer + 3 * sizeof(int)) = d;
 }
